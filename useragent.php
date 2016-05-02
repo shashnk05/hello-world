7d984fcd3739161b0b5f806cd87debb9c00ba75b
shashank <shasha@gmail.com>
2016-05-02 14:40:12 +0530

    added a file through bash

---
+++ useragent.php
@@ -0,0 +1,6 @@
+<?php if(strstr($_SERVER['HTTP_USER_AGENT'],'iPhone') || strstr($_SERVER['HTTP_USER_AGENT'],'iPod') || strstr($_SERVER['HTTP_USER_AGENT'],'iPad') || strstr($_SERVER['HTTP_USER_AGENT'],'Android') || strstr($_SERVER['HTTP_USER_AGENT'],'BlackBerry'))
+ { echo 'iphone';
+ }else{
+	 
+	 echo 'hfgh';
+ }?>
